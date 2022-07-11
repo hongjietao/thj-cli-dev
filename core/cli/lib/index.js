43a112a2e49e6140ba15d67d1d53481dd5fd5519
陶鸿杰 <18790315365@163.com>
2022-07-12 00:33:46 +0800

    feat: 检查环境变量，创建环境变量done

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,19 +2,73 @@
 
 module.exports = core;
 
+const path = require("path");
 const semver = require("semver");
 const colors = require("colors/safe");
-const pkg = require("../package.json");
+const userHome = require("user-home");
+const pathExists = require("path-exists").sync;
 const log = require("@thj-cli-dev/log");
+
+const pkg = require("../package.json");
 const constant = require("./constant");
+let config;
 
 function core() {
   try {
     checkPkgVersion();
     checkNodeVersion();
     checkRoot();
+    checkUserHome();
+    checkEnv();
   } catch (e) {
     log.error(e.message);
+  }
+}
+
+/**
+ * Checks env is exists
+ *
+ * 检查环境变量
+ */
+function checkEnv() {
+  const dotenv = require("dotenv");
+  const dotEnvPath = path.resolve(userHome, ".env");
+  if (pathExists(dotEnvPath)) {
+    config = dotenv.config({
+      path: dotEnvPath,
+    });
+  }
+  createDefaultConfig();
+  log.info("环境变量：", process.env.CLI_CONFIG_PATH);
+}
+
+/**
+ * create default env config
+ *
+ * 创建默认脚手架环境变量配置
+ */
+function createDefaultConfig() {
+  const cliConfig = {
+    home: userHome,
+  };
+
+  if (process.env.CLI_HOME) {
+    cliConfig["cliHome"] = path.join(userHome, process.env.CLI_HOME);
+  } else {
+    cliConfig["cliHome"] = path.join(userHome, constant.DEFAULT_CLI_HOME);
+  }
+
+  process.env.CLI_CONFIG_PATH = cliConfig.cliHome;
+}
+
+/**
+ * Checks user home directory is exists
+ *
+ * 检查用户主目录是否存在
+ */
+function checkUserHome() {
+  if (!userHome || !pathExists(userHome)) {
+    throw new Error(colors.red("当前登录用户主目录不存在！"));
   }
 }
 
@@ -26,8 +80,6 @@ function core() {
 function checkRoot() {
   const rootCheck = require("root-check");
   rootCheck();
-  // console.log(process.geteuid());
-  // log.info();
 }
 
 /**
@@ -36,7 +88,7 @@ function checkRoot() {
  * 检查当前版本号
  */
 function checkPkgVersion() {
-  log.notice(`version: `, pkg.version);
+  log.notice(`thj-cli version: `, pkg.version);
 }
 
 /**
@@ -46,7 +98,7 @@ function checkPkgVersion() {
  */
 function checkNodeVersion() {
   // 1. 获取当前版本号
-  log.notice(`node version: `, process.version);
+  // log.notice(`node version: `, process.version);
   const curNodeVersion = process.version;
   const lowestNodeVersion = constant.LOWEST_NODE_VERSION;
 
