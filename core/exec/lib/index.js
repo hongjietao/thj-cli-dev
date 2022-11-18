6bd86ef17a951a7825ce221a489f75dd8d03357c
陶鸿杰 <18790315365@163.com>
2022-11-19 02:01:16 +0800

    feat: week 4, lesson 4-4; Package类的属性、方法定义及构造函数逻辑开发,

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -1,13 +1,30 @@
 "use strict";
 
 const Package = require("@thj-cli-dev/package");
+const log = require("@thj-cli-dev/log");
 
-function exec() {
-  const pkg = new Package();
-  console.log("exec");
+const SETTTINGS = {
+  init: "@thj-cli-dev/init",
+};
+
+function exec(name, options, cmdObj) {
+  const targetPath = process.env.CLI_TARGET_PATH;
+  const homePath = process.env.CLI_HOME_PATH;
+  const packageName = SETTTINGS[cmdObj.name()];
+  const packageVersion = "latest";
+  log.verbose("homePath: ", process.env.CLI_HOME_PATH);
+  // const cmdObj = arguments[arguments.length - 1];
+  console.log(name, options, cmdObj.name());
+  // console.log(arguments, "arguments");
+  const pkg = new Package({
+    targetPath,
+    packageName,
+    packageVersion,
+  });
+
   console.log(pkg);
-  console.log(process.env.CLI_TARGET_PATH);
-  console.log(process.env.CLI_HOME_PATH);
 }
 
 module.exports = exec;
+
+// thj-cli-dev init -tp D:\\thj-cli-dev\\thj-cli-dev\\commands\\init  --debug
--- utils/utils/lib/index.js
+++ utils/utils/lib/index.js
@@ -1,8 +1,7 @@
 "use strict";
 
-module.exports = utils;
-
-function utils() {
-  // TODO
-  console.log("this is utils");
+function isObject(o) {
+  return Object.prototype.toString.call(o) === "[object Object]";
 }
+
+module.exports = { isObject };
