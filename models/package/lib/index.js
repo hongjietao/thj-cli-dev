a4a8bb503ce2da206ed529deaf1aac63427e989c
陶鸿杰 <18790315365@163.com>
2022-11-19 01:58:25 +0800

    feat: week 4, lesson 4-4; Package类的属性、方法定义及构造函数逻辑开发,

--- models/package/lib/index.js
+++ models/package/lib/index.js
@@ -1,9 +1,36 @@
 "use strict";
 
+const { isObject } = require("@thj-cli-dev/utils");
+
 class Package {
-  constructor() {
-    console.log("Package constructor");
+  constructor(options) {
+    if (!options) {
+      throw new Error("Package 类的 options 参数不能为空！");
+    }
+    if (!isObject(options)) {
+      throw new Error("Package 类的 options 参数必须为对象！");
+    }
+    // package 的路径
+    this.targetPath = options.targetPath;
+    // package 的存储路径
+    this.storePath = options.storePath;
+    // package 的 name
+    this.packageName = options.packageName;
+    // package 的 version
+    this.packageVersion = options.packageVersion;
   }
+
+  // 判断当前Package是否存在
+  exists() {}
+
+  // install package
+  install() {}
+
+  // update package
+  update() {}
+
+  // 获取入口文件的路径
+  getRootFilePath() {}
 }
 
 module.exports = Package;
